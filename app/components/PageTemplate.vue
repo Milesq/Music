448a7416a212f33634416ecd0403c5e4393c2dea
Milesq <wmilosz88@gmail.com>
2019-04-18 17:08:31 +0200

    Add font awesome

--- app/components/PageTemplate.vue
+++ app/components/PageTemplate.vue
@@ -11,9 +11,32 @@
     </ScrollView>
 
     <FlexboxLayout class="footer">
-      <Label @tap="$store.commit('push', '/')">Home</Label>
-      <Label @tap="$store.commit('push', 'lessons')">Lessons</Label>
-      <Label @tap="$store.commit('push', 'walking')">Walking</Label>
+      <FlexboxLayout @tap="$store.commit('push', '/')" class="menu__option">
+        <Label>
+          <FormattedString>
+            <Span class="fas menu__icon" text.decode="&#xf015;"/>
+          </FormattedString>
+        </Label>
+        <Label>Home</Label>
+      </FlexboxLayout>
+
+      <FlexboxLayout @tap="$store.commit('push', 'lessons')" class="menu__option">
+        <Label>
+          <FormattedString>
+            <Span class="fas menu__icon" text.decode="&#xf02e;"/>
+          </FormattedString>
+        </Label>
+        <Label>Lessons</Label>
+      </FlexboxLayout>
+
+      <FlexboxLayout @tap="$store.commit('push', 'walking')" class="menu__option">
+        <Label>
+          <FormattedString>
+            <Span class="fas menu__icon" text.decode="&#xf70c;"/>
+          </FormattedString>
+        </Label>
+        <Label>Walking</Label>
+      </FlexboxLayout>
     </FlexboxLayout>
   </DockLayout>
 </template>
@@ -43,5 +66,17 @@
 
 .title {
   color: black;
+}
+
+.menu {
+  &__icon {
+    color: white;
+    font-size: 25px;
+  }
+
+  &__option {
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
